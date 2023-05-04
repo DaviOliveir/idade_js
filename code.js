47917b00d218e89336bc3d3dddafa5dc798f9a63
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-05-03 22:42:19 -0300

    concluido

--- code.js
+++ code.js
@@ -1,47 +1,56 @@
 function confir() {
     let data = new Date();
     let ano = data.getFullYear();
-    let formularioAno = window.document.getElementById('ano');
+    let formularioAno = window.document.getElementsByName('ano');
     let resultado = window.document.getElementById('resto');
 
     if (formularioAno.value == 0 || formularioAno.value  > ano) {
         window.alert("Por favor digite um ano válido");
     } else {
-        let sexo = document.getElementsByTagName('sexo')
-        let idade = ano - Number (formularioAno.value)
+        let nascimento = Number(formularioAno[0].value)
+        let sexo = document.getElementsByName('sexo')  
+        let idade = ano - nascimento
         let genero = ''
+        let homem = 0
+        let mulher = 1
         let img = document.createElement('img')
         img.setAttribute('id', 'foto')
-        if(sexo[0]){
+        let valorSexo = '';
+        for (let i = 0; i < sexo.length; i++) {
+        if (sexo[i].checked) {
+            valorSexo = sexo[i].value;
+        }
+        }
+        if(valorSexo == homem){
             genero = 'homem'
 
-        if(idade >= 0 && idade <= 10){
+         } if( idade <= 10){    
             img.setAttribute('src', 'bebemasculino.jpg')
             //criança
-        }else if(idade <21){
+        }else if(idade >10 && idade <=21){
             img.setAttribute('src', 'jovemasculino.jpg')
             //jovem
-        }else if(idade <50){
+        }else if(idade >21 && idade <=50){
             img.setAttribute('src', 'adulto.jpg')
             //adulto
-        }else{
+        }else if (idade >50 ){
 
             img.setAttribute('src', 'velho.jpg')
             //idoso
         }
-            }else if(sexo[1]){
+           else if(valorSexo == mulher){
 
-               genero = mulher
+               genero = 'mulher'
 
-               if(idade >= 0 && idade <= 10){
+            } if(idade >= 0 && idade <= 10){
                 img.setAttribute('src', 'bebef.jpg')
 
                 //criança
-            }else if(idade <21){
-                img.setAttribute('src', 'jovemf.jpg')
+            }else if(idade>10 && idade <=21){
+                img.setAttribute('src', 'jovemf.jpg')   
     
                 //jovem
-            }else if(idade <50){
+            }else if(idade >21 && idade<=50){
                 img.setAttribute('src', 'adulta.jpg')
     
                 //adulto
@@ -52,12 +61,12 @@ function confir() {
 
 
 
-
-            }
         
-       resto.style.textAlign = 'center'
-        resto.innerHTML = `seu genero é ${genero} e sua idade é ${idade}`
-        res.appendChild(img)
+            
+        
+       resultado.style.textAlign = 'center'
+        resultado.innerHTML = `seu genero é ${genero} e sua idade é ${idade}`
+        resultado.appendChild(img)
     }
 }
 
